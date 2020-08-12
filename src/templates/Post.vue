8f9a4821b97e50a89eecd113533ebceedb19b506
Muhammad Hizbullah <muh-hizbe@users.noreply.github.com>
2020-08-12 08:40:01 +0700

    disable hyvor talk and enable gitalk plugin 0.0.8

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -31,8 +31,10 @@
 </template>
 
 <script>
-import 'vue-gitalk/dist/vue-gitalk.css'
-import Gitalk from 'vue-gitalk'
+// import 'vue-gitalk/dist/vue-gitalk.css'
+// import Gitalk from 'vue-gitalk'
+import 'gitalk/dist/gitalk.css'
+import Gitalk from 'gitalk'
 import PostMeta from '~/components/PostMeta'
 import PostTags from '~/components/PostTags'
 import Author from '~/components/Author.vue'
