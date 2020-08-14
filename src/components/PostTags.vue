4bf44ae2304fcef392a4be39f20cb9d43822dd98
Muhammad Hizbullah <muh-hizbe@users.noreply.github.com>
2020-08-14 13:16:38 +0700

    update PostTags component 0.0.1

--- src/components/PostTags.vue
+++ src/components/PostTags.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="post-tags">
    		<g-link class="post-tags__link" v-for="tag in post.tags" :key="tag.id" :to="tag.path">
-   			<span>#</span> {{ tag.title }}
+   			<span>#</span>{{ tag.title }}
    		</g-link>
     </div>
 </template>
