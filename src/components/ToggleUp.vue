db915790d747d3c6370a91e57c494e208358902c
Muhammad Hizbullah <muh-hizbe@users.noreply.github.com>
2020-10-06 13:24:00 +0700

    redesign toggle up

--- src/components/ToggleUp.vue
+++ src/components/ToggleUp.vue
@@ -1,8 +1,11 @@
 <template>
-	<a class="arrow-up" @click="toTop" id="to_top" v-show="visible" title="go to top">
-	  <span class="arrow-up__span">
-	    &uarr;
-	  </span>
+	<a class="arrow-up rounded-full inline-flex" @click="toTop" id="to_top" v-show="visible" title="scroll to top window">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path class="w-5 h-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z" />
+        </svg>
+        <span class="arrow-up__span ml-2">
+            Scroll Up
+        </span>
 	</a>
 </template>
 
@@ -49,7 +52,6 @@ export default {
     padding: 8px 15px;  
     background-color: transparent;
     border: 0;
-    border-radius: 0.5rem;
     color: var(--text-toggle);
     background-color: var(--bg-toggle);
     box-shadow: var(--box-shadow);
@@ -62,7 +64,11 @@ export default {
         outline: none;
     }
     &__span {
+        color: var(--text-toggle);
         font-size: 25px;
+    }
+    svg{
+        color: var(--text-toggle);
     }
 }
 </style>
