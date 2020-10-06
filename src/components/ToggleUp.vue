31b38d81e6d21c00b43ac628151f86f14f3b1d95
Muhammad Hizbullah <muh-hizbe@users.noreply.github.com>
2020-10-06 13:54:25 +0700

    add select-none class in to toggle up

--- src/components/ToggleUp.vue
+++ src/components/ToggleUp.vue
@@ -1,5 +1,5 @@
 <template>
-	<a class="arrow-up rounded-full inline-flex" @click="toTop" id="to_top" v-show="visible" title="scroll to top window">
+	<a class="arrow-up rounded-full inline-flex select-none" @click="toTop" id="to_top" v-show="visible" title="scroll to top window">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path class="w-5 h-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z" />
         </svg>
