27bec2f390452c33f2bc0d2f79296ae84f155c70
Muhammad Hizbullah <muh-hizbe@users.noreply.github.com>
2020-10-06 13:23:48 +0700

    redesign toggle theme

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -1,7 +1,7 @@
 <template>
-  <button role="button" aria-label="Toggle dark/light" @click.prevent="toggleTheme" class="toggle-theme" :class="darkTheme ? 'bg-light' : 'bg-dark'" title="change theme">
-      <svg v-if="darkTheme" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
-      <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+  <button role="button" aria-label="Toggle dark/light" @click.prevent="toggleTheme" class="toggle-theme rounded-full p-1 w-8 h-8" :class="darkTheme ? 'bg-light' : 'bg-dark'" title="change theme">
+      <svg v-if="darkTheme" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun w-6 h-6"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
+      <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon w-6 h-6"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
   </button>
 </template>
 
@@ -30,10 +30,8 @@ export default {
 
 <style lang="scss">
 .toggle-theme {
-  padding: 10px;  
   background-color: transparent;
   border: 0;
-  border-radius: 0.5rem;
   color: var(--text-toggle);
   cursor: pointer;
 
@@ -42,6 +40,9 @@ export default {
   }
   &:focus {
     outline: none;
+  }
+  svg{
+      vertical-align: none;
   }
 }
 .bg-light{
