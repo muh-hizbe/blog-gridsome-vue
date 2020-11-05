4be0d176de2f8bc17ef5365f01b0b0f55778e5ec
Muhammad Hizbullah <muh-hizbe@users.noreply.github.com>
2020-11-05 09:53:29 +0700

    use PostLayout as globally & reorder position meta

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import '@/assets/style/index.scss'
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '@/layouts/Default.vue'
+import PostLayout from '@/layouts/Post.vue'
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
     if (isClient && process.env.NODE_ENV === 'production') {
@@ -9,28 +10,6 @@ export default function (Vue, { router, head, isClient }) {
     }
 
     head.htmlAttrs = { lang: 'id' }
-
-    head.link.push({
-        rel: 'manifest',
-        href: '../manifest.json'
-    })
-
-    head.link.push({
-        rel: 'canonical',
-        href: 'https://hizbe.dev'
-    })
-
-    head.link.push({
-        rel: 'apple-touch-icon',
-        href: 'maskable_icon.png'
-    })
-
-    head.link.push({
-        rel: 'stylesheet',
-        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-        integrity: 'sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==',
-        crossorigin: 'anonymous'
-    })
 
     head.meta.push({
         name: 'keywords',
@@ -52,6 +31,29 @@ export default function (Vue, { router, head, isClient }) {
         content: 'width=device-width, initial-scale=1.0'
     })
 
+    head.link.push({
+        rel: 'manifest',
+        href: '../manifest.json'
+    })
+
+    head.link.push({
+        rel: 'canonical',
+        href: 'https://hizbe.dev'
+    })
+
+    head.link.push({
+        rel: 'apple-touch-icon',
+        href: 'maskable_icon.png'
+    })
+
+    head.link.push({
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+        integrity: 'sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==',
+        crossorigin: 'anonymous'
+    })    
+
     // Set default layout as a global component
     Vue.component('Layout', DefaultLayout)
+    Vue.component('PostLayout', PostLayout)
 }
